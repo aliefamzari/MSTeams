931a20519980a8a0613158da4b9d6a31cd6f17c2
aliefamzari <aliefamzari@gmail.com>
2022-09-15 22:55:41 +0800

    add remove cache function and chromium edge remove cache function, set global error action preference

--- TeamsReinstallThorough.ps1
+++ TeamsReinstallThorough.ps1
@@ -4,9 +4,74 @@
 ##################################
 ##################################
 #####      Initiation        #####
-#####    By XSIOL & TOBKO    #####
+#### By XSIOL & TOBKO & ALMAZ ####
 ##################################
 ##################################
+
+$ErrorActionPreference = SilentlyContinue
+#------------------------------------------------------------------#
+#- Remove-CacheFiles Function                                               #
+#------------------------------------------------------------------#
+Function Remove-CacheFiles {
+    param([Parameter(Mandatory=$true)][string]$path)    
+    BEGIN 
+    {
+        $originalVerbosePreference = $VerbosePreference
+        $VerbosePreference = 'Continue'  
+    }
+    PROCESS 
+    {
+        if((Test-Path $path))
+        {
+            if([System.IO.Directory]::Exists($path))
+            {
+                try 
+                {
+                    if($path[-1] -eq '\')
+                    {
+                        [int]$pathSubString = $path.ToCharArray().Count - 1
+                        $sanitizedPath = $path.SubString(0, $pathSubString)
+                        Remove-Item -Path "$sanitizedPath\*" -Recurse -Force  -Verbose
+                    }
+                    else 
+                    {
+                        Remove-Item -Path "$path\*" -Recurse -Force  -Verbose              
+                    } 
+                } catch { }
+            }
+            else 
+            {
+                try 
+                {
+                    Remove-Item -Path $path -Force  -Verbose
+                } catch { }
+            }
+        }    
+    }
+    END 
+    {
+        $VerbosePreference = $originalVerbosePreference
+    }
+}
+
+
+#------------------------------------------------------------------#
+#- Clear-EdgeCache Function                                                #
+#------------------------------------------------------------------#
+
+
+Function Clear-EdgeCache {
+    param([string]$user=$env:USERNAME)
+    if((Test-Path "C:\Users$user\AppData\Local\Microsoft\Edge\User Data\Default"))
+    {
+        $EdgeAppData = "C:\Users$user\AppData\Local\Microsoft\Edge\User Data\Default"
+        $possibleCachePaths = @('Cache','Cache2\entries','Cookies','History','Top Sites','Visited Links','Web Data','Media History','Cookies-Journal')
+        ForEach($cachePath in $possibleCachePaths)
+        {
+            Remove-CacheFiles "$EdgeAppData$cachePath"
+        }
+        }
+}
 
 $challenge = Read-Host "Are you sure you wish to completely reinstall Microsoft Teams? 
 This will also close Internet Explorer, Chrome, Firefox & Edge (Y/N)"
@@ -43,7 +108,7 @@ elseif ($challenge -eq "Y"){
     Write-Host "Stopping Teams Process" -ForegroundColor Yellow
 
     try{
-        Get-Process -ProcessName Teams -ErrorAction SilentlyContinue | Stop-Process -Force
+        Get-Process -ProcessName Teams  | Stop-Process -Force
         Start-Sleep -Seconds 3
         Write-Host "Teams Process Sucessfully Stopped" -ForegroundColor Green
     }
@@ -65,7 +130,7 @@ elseif ($challenge -eq "Y"){
         }
         if (Test-Path -Path $TeamsPath) {
             Write-Host "Deleting Teams directory"
-            Remove-Item –Path $TeamsPath -Recurse
+            Remove-Item ï¿½Path $TeamsPath -Recurse
         }
     }
     catch
@@ -83,14 +148,14 @@ elseif ($challenge -eq "Y"){
     Write-Host "Clearing Teams Disk Cache" -ForegroundColor Yellow
     
     try{
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\application cache\cache" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\blob_storage" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\databases" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\cache" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\gpucache" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Indexeddb" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Local Storage" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\tmp" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\application cache\cache"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\blob_storage"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\databases"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\cache"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\gpucache"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Indexeddb"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Local Storage"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\tmp"  | Remove-Item -Confirm:$false 
         Write-Host "Teams Disk Cache Cleaned" -ForegroundColor Green
     }
     
@@ -101,7 +166,7 @@ elseif ($challenge -eq "Y"){
     Write-Host "Stopping Chrome Process" -ForegroundColor Yellow
 
     try{
-        Get-Process -ProcessName Chrome -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+        Get-Process -ProcessName Chrome  | Stop-Process -Force 
         Start-Sleep -Seconds 3
         Write-Host "Chrome Process Sucessfully Stopped" -ForegroundColor Green
     }
@@ -113,9 +178,9 @@ elseif ($challenge -eq "Y"){
     Write-Host "Clearing Chrome Cache" -ForegroundColor Yellow
     
     try{
-        Get-ChildItem -Path $env:LOCALAPPDATA"\Google\Chrome\User Data\Default\Cache" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:LOCALAPPDATA"\Google\Chrome\User Data\Default\Cookies" -File -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
-        Get-ChildItem -Path $env:LOCALAPPDATA"\Google\Chrome\User Data\Default\Web Data" -File -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -ErrorAction SilentlyContinue
+        Get-ChildItem -Path $env:LOCALAPPDATA"\Google\Chrome\User Data\Default\Cache"  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:LOCALAPPDATA"\Google\Chrome\User Data\Default\Cookies" -File  | Remove-Item -Confirm:$false 
+        Get-ChildItem -Path $env:LOCALAPPDATA"\Google\Chrome\User Data\Default\Web Data" -File  | Remove-Item -Confirm:$false 
         Write-Host "Chrome Cleaned" -ForegroundColor Green
     }
     
@@ -126,9 +191,9 @@ elseif ($challenge -eq "Y"){
     Write-Host "Stopping IE & Edge Process" -ForegroundColor Yellow
     
     try{
-        Get-Process -ProcessName MicrosoftEdge -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-        Get-Process -ProcessName MSEdge -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
-        Get-Process -ProcessName IExplore -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+        Get-Process -ProcessName MicrosoftEdge  | Stop-Process -Force 
+        Get-Process -ProcessName MSEdge  | Stop-Process -Force 
+        Get-Process -ProcessName IExplore  | Stop-Process -Force 
         Write-Host "Internet Explorer and Edge Processes Sucessfully Stopped" -ForegroundColor Green
     }
     
@@ -141,6 +206,7 @@ elseif ($challenge -eq "Y"){
     try{
         RunDll32.exe InetCpl.cpl, ClearMyTracksByProcess 8
         RunDll32.exe InetCpl.cpl, ClearMyTracksByProcess 2
+        Clear-EdgeCache
         Start-Sleep 3
         Write-Host "IE and Edge Cleaned" -ForegroundColor Green
     }
@@ -152,7 +218,7 @@ elseif ($challenge -eq "Y"){
     Write-Host "Stopping Firefox Process" -ForegroundColor Yellow
 
     try{
-        Get-Process -ProcessName Firefox -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue
+        Get-Process -ProcessName Firefox  | Stop-Process -Force 
         Start-Sleep -Seconds 3
         Write-Host "Firefox Process Sucessfully Stopped" -ForegroundColor Green
     }
@@ -164,7 +230,7 @@ elseif ($challenge -eq "Y"){
     Write-Host "Clearing Firefox Cache" -ForegroundColor Yellow
     
     try{
-        Get-ChildItem -Path $env:LOCALAPPDATA"\Mozilla\Firefox\Profiles\" -ErrorAction SilentlyContinue | Remove-Item -Confirm:$false -Recurse -ErrorAction SilentlyContinue
+        Get-ChildItem -Path $env:LOCALAPPDATA"\Mozilla\Firefox\Profiles\"  | Remove-Item -Confirm:$false -Recurse 
 
         Write-Host "Firefox Cleaned" -ForegroundColor Green
     }
